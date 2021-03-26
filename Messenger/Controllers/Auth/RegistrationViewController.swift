da3af0ba9bd8cf2a9d476b84b266fe0f882f6673
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-03-26 21:13:27 +0800

    feat: partially build login screen

---
+++ Messenger/Controllers/Auth/RegistrationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationViewController.swift
+//  Messenger
+//
+//  Created by Wang on 26/3/21.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+}
